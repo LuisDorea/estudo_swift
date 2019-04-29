03d3faca9e7634798eaa39cbe857f2ade5fa00e8
Luis Dorea <luis.dorea09@gmail.com>
2019-04-29 17:35:39 -0300

    trasição entre telas

---
+++ Segues/Segues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by Temet Desenvolvimento on 29/04/19.
+//  Copyright © 2019 Luis Dorea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
